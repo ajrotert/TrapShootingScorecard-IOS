c5a550f4f1dca77d8fb5e5caae433753137ca4cd
Andrew Rotert <Andrew.Rotert@trojans.dsu.edu>
2019-01-14 22:23:19 -0600

    Progress Made 1/14
    Kind of works, still need to complete features, like the mini map, and the player names

---
+++ Shooter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AR.TrapScorecard
+{
+    public class Shooter
+    {
+        public Shooter(string name)
+        {
+            Name = name;
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+
+        public int shot = 0;
+        public int[] score = new int[25];
+
+        public void UpdateScore(bool hit)
+        {
+            if (hit)
+                score[shot] = 1;
+            else
+                score[shot] = 0;
+            shot++;
+        }
+        public int GetTotal()
+        {
+            int total = 0;
+            if(shot == 25)
+            {
+                for (int a = 0; a < score.Length; a++)
+                    total += score[a];
+                return total;
+            }
+            return -1;
+
+        }
+    }
+}
---
+++ ViewController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+using UIKit;
+
+namespace AR.TrapScorecard
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public Shooter[] shooters;
+        UITextField NameInputTextBox = new UITextField();
+        int current = 0;
+        int pos = 0;
+        int max;
+        
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            HitButton.Enabled = false;
+            MissButton.Enabled = false;
+            PauseButton.Enabled = false;
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }  
+
+        partial void Pause_Clicked(UIButton sender)
+        {
+            if(!HitButton.Enabled)
+            {
+                HitButton.Enabled = true;
+                MissButton.Enabled = true;
+                PauseButton.SetTitle("Pause", UIControlState.Normal);
+            }
+            else
+            {
+                HitButton.Enabled = false;
+                MissButton.Enabled = false;
+                PauseButton.SetTitle("Resume", UIControlState.Normal);
+            }
+        }
+
+        partial void Start_Clicked(UIButton sender)
+        { 
+            max = Convert.ToInt32(NumberOfShooterSlider.Value);
+            shooters = new Shooter[max];
+            NumberOfShooterSlider.Enabled = false;
+            for (int a = 0; a < max; a++)
+            {
+                shooters[a] = new Shooter("Tesing Development");
+            }
+            StartButton.Enabled = false;
+            PauseButton.Enabled = true;
+            MissButton.Enabled = true;
+            HitButton.Enabled = true;
+        }
+        public void GetText(string title, string message)
+        {
+            UIAlertView alert = new UIAlertView()
+            {
+                Title = title,
+                //Title = "Enter the shooters names",
+                Message = message
+            };
+            //alert.AlertViewStyle = UIAlertViewStyle.PlainTextInput;
+
+            //NameInputTextBox = alert.GetTextField(0);
+            //alert.Clicked += delegate (object s, UIButtonEventArgs e)
+            //{
+                //test = getTexField(0).Text//doesn't set text until the button is clicked, does not reutrn anything
+            //};
+            alert.AddButton("OK");
+            alert.Show();
+        }
+
+        partial void Slider_Changed(UISlider sender)
+        {
+            NumberOfShooterSlider.Value = (float)Math.Floor(NumberOfShooterSlider.Value);
+            NumShootersLabel.Text = "Shooters: " + NumberOfShooterSlider.Value.ToString();
+        }
+
+        partial void Hit_Clicked(UIButton sender)
+        {
+            shooters[pos].UpdateScore(true); //Shooter.Current
+            Update();
+        }
+
+        partial void Miss_Clicked(UIButton sender)
+        {
+            shooters[pos].UpdateScore(false);
+            Update();
+        }
+        public void Update()
+        {
+            if (current == (max * 25) -1)
+            {
+                End();
+            }
+            else
+            {
+                if (current % (max * 5) == 0 && current != 0)
+                    GetText("Rotate", null);
+                pos++;
+                current++;
+                if (pos == max)
+                    pos = 0;
+            }
+        }
+        public void End()
+        {
+            HitButton.Enabled = false;
+            MissButton.Enabled = false;
+            PauseButton.Enabled = false;
+
+            string message = "";
+            string title = "Shooter:\t\tScore:";
+            for(int a =0; a<max; a++)
+            {
+                message += "\n" + shooters[a].Name + "\t\t" + shooters[a].GetTotal().ToString();
+            }
+            GetText(title, message);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
