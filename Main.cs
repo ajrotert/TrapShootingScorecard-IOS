03a44565f11c6118abbd6c0e30c7dcf49e0cc81f
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-02-05 11:42:19 -0600

    Connecting with github

--- Main.cs
+++ Main.cs
@@ -5,6 +5,7 @@ namespace AR.TrapScorecard
     public class Application
     {
         // This is the main entry point of the application.
+        // This file is not usually changed
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
